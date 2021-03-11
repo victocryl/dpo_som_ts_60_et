024c01455d964ba7213299a4a93a8313f4b0c9ef
victor <vikr1@yandex.ru>
2021-03-11 16:37:04 +0300

    УКВ1. Статусы. Впервые сделал уст. галочки по биту в посылке rx264

--- tab_statuses.py
+++ tab_statuses.py
@@ -8,4 +8,19 @@ class Statuses:
         self.mainwind = mainwind
 
     def statuses_reading(self):
-        pass
+        ############# УКВ 1 ####################################################################
+        # бит Приточный вентилятор 1 включен
+        if (self.mainwind.Can_cor.rx_ukv_1_2[0] & sub.BIT0):
+            self.mainwind.checkBox_141.setCheckState(Qt.Checked)
+            self.mainwind.checkBox_52.setCheckState(Qt.Checked)
+        else:
+            self.mainwind.checkBox_141.setCheckState(Qt.Unchecked)
+            self.mainwind.checkBox_52.setCheckState(Qt.Unchecked)
+        
+        # бит Приточный вентилятор 2 включен
+        if (self.mainwind.Can_cor.rx_ukv_1_2[0] & sub.BIT1):
+            self.mainwind.checkBox_144.setCheckState(Qt.Checked)
+            self.mainwind.checkBox_54.setCheckState(Qt.Checked)
+        else:
+            self.mainwind.checkBox_144.setCheckState(Qt.Unchecked)
+            self.mainwind.checkBox_54.setCheckState(Qt.Unchecked)
